899d4134295e732190805a00404bd193110d50ea
zhangbo8264 <zhangbo8264@163.com>
2015-07-31 15:29:08 +0800

    Signed-off-by: zhangbo8264 <zhangbo8264@163.com>

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,15 @@ module.exports = function(app){
 	app.get('/',function(req,res){
 		res.render('index',{title:'首页'});
 	});
+
+	app.get('/chat',function(req,res){
+		res.render('chat',{title:'好友聊天'});
+	});
+
+
+	app.get('/work',function(req,res){
+		res.render('work',{title:'创作'});
+	})
+
 }
  
